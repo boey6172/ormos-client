30f419bc1e515d3ab45b8a5eac38ac66c4047555
Daniel Mabunga <boey6172@gmail.com>
2020-12-07 22:56:27 +0800

    ignore remove for config

---
+++ src/config/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCl7gGiwwOPR8rNmJSQ09Vp0lUPrOVkI3Q",
+  authDomain: "testing-49c7b.firebaseapp.com",
+  databaseURL: "https://testing-49c7b.firebaseio.com",
+  projectId: "testing-49c7b",
+  storageBucket: "testing-49c7b.appspot.com",
+  messagingSenderId: "610079685602",
+  appId: "1:610079685602:web:3bbb6d28808951f52917bb",
+  measurementId: "G-ZL0P2YX073"
+}
+
+const fire = firebase.initializeApp(firebaseConfig);
+
+
+
+export default fire
