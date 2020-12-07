c7b5bf646eb2b25ffa2210b2a7a23445b102bd74
Daniel Mabunga <boey6172@gmail.com>
2020-12-07 22:50:03 +0800

    leaflet map

--- src/services/map.js
+++ src/services/map.js
@@ -1,29 +1,42 @@
 import React, {useState,useRef} from 'react';
-import { MapContainer,TileLayer,Popup,Marker,useMapEvents,useMap,useMapEvent,Rectangle} from "react-leaflet";
+import { MapContainer,TileLayer,Popup,Marker,useMapEvent,useMap,Rectangle} from "react-leaflet";
 import { Button,ButtonGroup,IconButton,Divider,Paper,InputBase,Container } from '@material-ui/core';
 import L from "leaflet";
 import '../leaflet/leaflet.css';
 
-const LocationMarker =()=> {
+const PtopMap =()=> {
   const [center, setCenter] = useState({lat: 13.4113, lng: 121.1808})
   const ZOOM_LEVEL = 13;
   // const mapRef = useRef();
-  const position = [13.4113, 121.1808]
+  const [position, setPosition] = useState({lat: 13.4113, lng: 121.1808})
+
+  const locationMarker=()=> {
+    // const [position, setPosition] = useState(null)
+
+  
+    return position === null ? null : (
+      <Marker position={position}>
+        <Popup>You are here</Popup>
+      </Marker>
+    )
+  }
 
   
 return(
   <>
+  <button onClick={locationMarker}>locate</button>
   <MapContainer
     center={center}
     zoom={ZOOM_LEVEL}
     // ref={mapRef}
+  
   >
     <TileLayer url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=B7Qswco9rqEFCQ4fnonV" />
-    <Marker position={position}>
+    {/* <Marker position={position}>
       <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
+        You are here <br /> 
       </Popup>
-    </Marker>
+    </Marker> */}
 
   </MapContainer>
 
@@ -31,4 +44,4 @@ return(
 )
 }
 
-export default LocationMarker
+export default PtopMap
